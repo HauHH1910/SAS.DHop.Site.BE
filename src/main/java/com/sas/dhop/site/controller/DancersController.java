7da18885df6d837ce8934a1216216590cace03f1
kouhako <ty30012003@gmail.com>
2025-06-04 18:05:37 +0700

    Get All Dancer services

--- src/main/java/com/sas/dhop/site/controller/DancersController.java
+++ src/main/java/com/sas/dhop/site/controller/DancersController.java
@@ -7,6 +7,7 @@ import com.sas.dhop.site.service.DancerService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ public class DancersController {
 
     private final DancerService dancerService;
 
+    @GetMapping
     public ResponseData<List<DancerResponse>> getAllDancer(){
         return ResponseData.<List<DancerResponse>>builder()
                 .message(ResponseMessage.GET_ALL_DANCERS)
