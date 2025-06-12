da3eb1e1dcad69eb3bd23e8d8a1288ef259eb056
kouhako <ty30012003@gmail.com>
2025-06-12 18:44:42 +0700

    ⠙⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⢹⠿⣿⣿⣿⣿⣿ ⣷⣶⡀⠿⠿⣿⣿⣿⣿⣿⣿⡇⠐⠂⢒⡢⠀⣿⣿⣿ ⣿⣿⣿⣆⠀⠈⢻⣿⣿⣿⣿⣿⡆⢈⣿⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⣷⣄⠀⠙⠻⢻⢿⣿⠷⢠⢽⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣧⡀⠁⠀⢘⣱⣍⠿⣾⢿⣿⢿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⢉⢷⣌⠳⣿⣽⣛⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠋⠽⠶⡌⣿⣻⣀ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⣠⡀⠀⠀⠀⠐⠇⢿⣿⣿ ⠿⠿⠿⠿⠿⠿⠿⠿⠏⠁⠀⠈⠀⠅⠶⠲⠶⠆⠔⠿
    Hitler can code article API

--- src/main/java/com/sas/dhop/site/dto/response/AreaResponse.java
+++ src/main/java/com/sas/dhop/site/dto/response/AreaResponse.java
@@ -2,10 +2,10 @@ package com.sas.dhop.site.dto.response;
 
 import com.sas.dhop.site.model.Area;
 
-public record AreaResponse(String district, String ward, String city) {
+public record AreaResponse(String district, String ward, String city, Integer areaId) {
 
     public static AreaResponse mapToAreaResponse(Area area){
-        return new AreaResponse(area.getDistrict(), area.getWard(), area.getCity());
+        return new AreaResponse(area.getDistrict(), area.getWard(), area.getCity(), area.getId());
     }
 
 }
