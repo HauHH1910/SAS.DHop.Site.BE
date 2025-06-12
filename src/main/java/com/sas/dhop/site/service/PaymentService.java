9eb6c8007275b33fd3e339d67a4f0da0ba18e0ea
HauHH1910 <hhhau1910@gmail.com>
2025-06-12 18:50:11 +0700

    update dancer

--- src/main/java/com/sas/dhop/site/service/PaymentService.java
+++ src/main/java/com/sas/dhop/site/service/PaymentService.java
@@ -2,13 +2,12 @@ package com.sas.dhop.site.service;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.sas.dhop.site.dto.request.CreatePaymentRequest;
-import vn.payos.type.CheckoutResponseData;
 
 import java.util.Map;
 
 public interface PaymentService {
 
-    CheckoutResponseData createPaymentLink(CreatePaymentRequest request);
+    ObjectNode createPaymentLink(CreatePaymentRequest request);
 
     ObjectNode getOrderByID(Long orderId);
 
