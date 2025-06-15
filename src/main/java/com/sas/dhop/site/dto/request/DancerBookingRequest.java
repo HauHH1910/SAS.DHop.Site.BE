6c5ed008a1a6e8610eeca3897c664ff011234232
HauHH1910 <hhhau1910@gmail.com>
2025-06-16 00:00:43 +0700

    update booking

--- src/main/java/com/sas/dhop/site/dto/request/DancerBookingRequest.java
+++ src/main/java/com/sas/dhop/site/dto/request/DancerBookingRequest.java
@@ -1,21 +1,17 @@
 package com.sas.dhop.site.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.NotNull;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
 import static com.sas.dhop.site.exception.MessageKey.DANCER_END_TIME_NOT_NULL;
 import static com.sas.dhop.site.exception.MessageKey.DANCER_START_TIME_NOT_NULL;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public record DancerBookingRequest(
-        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
-        @NotNull(message = DANCER_START_TIME_NOT_NULL)
-        LocalDateTime startTime,
-        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
-        @NotNull(message = DANCER_END_TIME_NOT_NULL)
-        LocalDateTime endTime,
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm") @NotNull(message = DANCER_START_TIME_NOT_NULL)
+                LocalDateTime startTime,
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm") @NotNull(message = DANCER_END_TIME_NOT_NULL) LocalDateTime endTime,
         String address,
         String detail,
         Integer dancerId,
@@ -24,6 +20,4 @@ public record DancerBookingRequest(
         BigDecimal bookingPrice,
         String dancerPhone,
         String customerPhone,
-        Integer teamSize
-) {
-}
+        Integer teamSize) {}
