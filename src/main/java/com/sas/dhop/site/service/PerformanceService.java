ff3ddcbf56dd6a422e79a142e7970ce635b72186
HauHH1910 <hhhau1910@gmail.com>
2025-06-15 18:07:32 +0700

    change code formatting to Java Google Format

---
+++ src/main/java/com/sas/dhop/site/service/PerformanceService.java
@@ -0,0 +1,13 @@
+package com.sas.dhop.site.service;
+
+import com.sas.dhop.site.dto.request.PerformanceRequest;
+import com.sas.dhop.site.dto.response.PerformanceResponse;
+
+public interface PerformanceService {
+
+  PerformanceResponse createPerformance(PerformanceRequest request);
+
+  PerformanceResponse getPerformanceById(Integer id);
+
+  void deletePerformanceById(Integer id);
+}
