70ad751acd219dca149177afc8401c6295f36ed7
HauHH1910 <hhhau1910@gmail.com>
2025-06-16 04:49:33 +0700

    very long night sir #2

--- src/main/java/com/sas/dhop/site/service/impl/ServerSentEventServiceImpl.java
+++ src/main/java/com/sas/dhop/site/service/impl/ServerSentEventServiceImpl.java
@@ -1,8 +1,6 @@
 package com.sas.dhop.site.service.impl;
 
 import com.sas.dhop.site.service.ServerSentEventService;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,26 +12,6 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public class ServerSentEventServiceImpl implements ServerSentEventService {
     @Override
     public SseEmitter subscribe() {
-        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE); // Or set a reasonable timeout
-
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        executor.execute(() -> {
-            try {
-                notificationService.addEmitter(emitter);
-            } catch (Exception e) {
-                emitter.completeWithError(e);
-            } finally {
-                executor.shutdown();
-            }
-        });
-
-        emitter.onCompletion(() -> notificationService.removeEmitter(emitter));
-        emitter.onTimeout(() -> {
-            notificationService.removeEmitter(emitter);
-            emitter.complete();
-        });
-        emitter.onError((e) -> notificationService.removeEmitter(emitter));
-
-        return emitter;
+        return null;
     }
 }
