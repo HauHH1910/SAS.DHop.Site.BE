1a9154ea3ef864b10404474863777e58a1da1ef3
HauHH1910 <hhhau1910@gmail.com>
2025-06-15 15:52:01 +0700

    spotless apply

---
+++ src/main/java/com/sas/dhop/site/dto/response/CancelBookingResponse.java
@@ -0,0 +1,5 @@
+package com.sas.dhop.site.dto.response;
+
+import java.time.LocalDateTime;
+
+public record CancelBookingResponse(Integer bookingId, String reason, String cancelPerson, LocalDateTime cancelDate) {}
