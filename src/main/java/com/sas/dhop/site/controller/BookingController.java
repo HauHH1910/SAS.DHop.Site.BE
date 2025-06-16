dd2326a9631aef44c0735dc55131cd5a33005db1
HauHH1910 <hhhau1910@gmail.com>
2025-06-16 14:16:25 +0700

    fix booking controller

--- src/main/java/com/sas/dhop/site/controller/BookingController.java
+++ src/main/java/com/sas/dhop/site/controller/BookingController.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
