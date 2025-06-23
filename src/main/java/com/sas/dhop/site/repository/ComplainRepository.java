aab655b90d2e0d9702ad9dad05db776ea43d56b7
kouhako <ty30012003@gmail.com>
2025-06-23 13:24:17 +0700

    Complains features not done yet.
    Nigga run

--- src/main/java/com/sas/dhop/site/repository/ComplainRepository.java
+++ src/main/java/com/sas/dhop/site/repository/ComplainRepository.java
@@ -1,8 +1,15 @@
 package com.sas.dhop.site.repository;
 
+import com.sas.dhop.site.model.Booking;
 import com.sas.dhop.site.model.Complain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ComplainRepository extends JpaRepository<Complain, Integer> {}
+public interface ComplainRepository extends JpaRepository<Complain, Integer> {
+
+    Optional<Complain> findByBooking(Booking booking);
+
+}
