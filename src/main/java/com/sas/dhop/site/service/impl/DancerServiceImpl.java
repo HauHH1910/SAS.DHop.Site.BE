2afc641463f2852a3e340ed2416438a977d65e74
kouhako <ty30012003@gmail.com>
2025-06-25 07:34:26 +0700

    Delete performanceId from booking table

--- src/main/java/com/sas/dhop/site/service/impl/DancerServiceImpl.java
+++ src/main/java/com/sas/dhop/site/service/impl/DancerServiceImpl.java
@@ -21,7 +21,6 @@ import com.sas.dhop.site.util.mapper.DancerMapper;
 import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
