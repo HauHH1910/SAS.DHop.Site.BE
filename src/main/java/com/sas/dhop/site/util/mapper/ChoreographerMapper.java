19c466ea17bd0b8f86ac955ce15cdfa6a87fc415
HauHH1910 <hhhau1910@gmail.com>
2025-07-16 05:10:40 +0700

    fix: ignore null JSON properties

--- src/main/java/com/sas/dhop/site/util/mapper/ChoreographerMapper.java
+++ src/main/java/com/sas/dhop/site/util/mapper/ChoreographerMapper.java
@@ -4,9 +4,11 @@ import com.sas.dhop.site.dto.request.ChoreographerRequest;
 import com.sas.dhop.site.dto.response.ChoreographerResponse;
 import com.sas.dhop.site.model.Choreography;
 import com.sas.dhop.site.model.DanceType;
+
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
@@ -20,18 +22,20 @@ public interface ChoreographerMapper {
     @Mapping(target = "status", ignore = true)
     Choreography mapToChoreography(ChoreographerRequest request);
 
-    @Mapping(target = "danceTypeId", source = "danceTypes", qualifiedByName = "mapDanceTypeToId")
+    @Mapping(target = "danceTypeName", source = "danceTypes", qualifiedByName = "mapDanceTypeToName")
     @Mapping(target = "userId", source = "user.id")
     @Mapping(target = "statusId", source = "status.id")
     @Mapping(target = "userName", source = "user.name")
     ChoreographerResponse mapToChoreographerResponse(Choreography choreography);
 
-    @Named("mapDanceTypeToId")
-    default List<Integer> mapDanceTypeToId(Set<DanceType> danceTypes) {
+    @Named("mapDanceTypeToName")
+    default List<String> mapDanceTypeToName(Set<DanceType> danceTypes) {
         if (danceTypes == null) {
             return List.of();
         }
-        return danceTypes.stream().map(DanceType::getId).collect(Collectors.toList());
+        return danceTypes.stream()
+                .map(DanceType::getType)
+                .toList();
     }
 
     @Mapping(target = "danceTypes", ignore = true)
